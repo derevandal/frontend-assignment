a2aa6b75019d956965e446bc68de60210222f525
André Van Dal <me@andrevandal.dev>
2019-08-17 22:47:50 -0300

    chore: rewrite imports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import 'Styles/main.scss';
 import Vue from 'vue'
-import App from './App.vue'
-import router from 'Router/index.js'
+import App from './App'
+import router from './router'
 
 import store from './store'
 
