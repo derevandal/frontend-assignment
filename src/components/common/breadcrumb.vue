ef172d802f5af65145345f3c1b58f5716912e33d
André Van Dal <me@andrevandal.dev>
2019-08-18 00:58:46 -0300

    feat: finish breadcrumb components

--- src/components/common/breadcrumb.vue
+++ src/components/common/breadcrumb.vue
@@ -1,5 +1,37 @@
 <template>
-  <div>
-    Breadcrumb
-  </div>
+  <ul class="Breadcrumb">
+    <li
+      v-for="({ name, link }, index) of breadcrumbs"
+      :key="`breadcrumb-${index}`"
+      class="Breadcrumb__item"
+    >
+      <router-link :to="link" class="Breadcrumb__link">
+        {{ name }}
+      </router-link>
+    </li>
+  </ul>
 </template>
+
+<script>
+export default {
+  name: 'CommonBreadcrumb',
+  data: () => ({
+    breadcrumbs: []
+  }),
+  watch: {
+    watch: {
+      $route() {
+        this.setBreadcrumbs()
+      }
+    }
+  },
+  beforeMount() {
+    this.setBreadcrumbs()
+  },
+  methods: {
+    setBreadcrumbs() {
+      this.breadcrumbs = this.$route.meta.breadcrumb
+    }
+  }
+}
+</script>
