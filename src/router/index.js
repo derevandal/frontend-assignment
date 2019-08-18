51c2e67895f6f9b8435b90ab0dc8d238fd7648fc
André Van Dal <me@andrevandal.dev>
2019-08-18 00:58:27 -0300

    feat: add meta breadcrumb to router

--- src/router/index.js
+++ src/router/index.js
@@ -5,9 +5,50 @@ import CompanyData from 'Pages/company-data.vue'
 import CompanyPage from 'Pages/company-page.vue'
 
 const routes = [
-  { path: '/', redirect: '/company-data' },
-  { path: '/company-data', component: CompanyData },
-  { path: '/company-page', component: CompanyPage }
+  {
+    path: '/',
+    redirect: '/company-data',
+    meta: {
+      breadcrumb: [
+        {
+          name: 'Home',
+          link: '/'
+        }
+      ]
+    }
+  },
+  {
+    path: '/company-data',
+    component: CompanyData,
+    meta: {
+      breadcrumb: [
+        {
+          name: 'Home',
+          link: '/'
+        },
+        {
+          name: 'Company Data',
+          link: 'company-data'
+        }
+      ]
+    }
+  },
+  {
+    path: '/company-page',
+    component: CompanyPage,
+    meta: {
+      breadcrumb: [
+        {
+          name: 'Home',
+          link: '/'
+        },
+        {
+          name: 'Company Page',
+          link: 'company-page'
+        }
+      ]
+    }
+  }
 ]
 
 Vue.use(Router)
