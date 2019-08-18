2108d48fc2735c01ef73c9b2ada104e78838b473
André Van Dal <me@andrevandal.dev>
2019-08-18 01:29:49 -0300

    feat: finish navigation

--- src/components/common/navigation.vue
+++ src/components/common/navigation.vue
@@ -1,5 +1,26 @@
 <template>
-  <div>
-    Navigation
-  </div>
+  <nav class="Navigation">
+    <router-link
+      v-for="({ to, name }, index) of items"
+      :key="`Navigation__link--${index}`"
+      :to="to"
+      :title="name"
+      class="Navigation__link"
+      active-class="Navigation__link--actived"
+      exact-active-class="Navigation__link--exact"
+      >{{ name }}</router-link
+    >
+  </nav>
 </template>
+<script>
+export default {
+  name: 'CommonNavigation',
+  data: () => ({
+    items: [
+      { to: '/company-data', name: 'Company Data' },
+      { to: '/', name: 'Company Table' },
+      { to: '/company-page', name: 'Company Page' }
+    ]
+  })
+}
+</script>
