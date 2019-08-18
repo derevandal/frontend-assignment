1d87f20379696a07a50f5921308cf189dae0b4a3
André Van Dal <me@andrevandal.dev>
2019-08-17 22:47:14 -0300

    feat: install vuex

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from 'Router/index.js'
 
+import store from './store'
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
   render: h => h(App)
 })
