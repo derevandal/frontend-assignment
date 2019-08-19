1a6ca2fe034ee6aef7d81c798dbe8b91c42cc4cc
André Van Dal <me@andrevandal.dev>
2019-08-19 13:42:43 -0300

    feat(notes): add modal working

--- src/components/company/form.vue
+++ src/components/company/form.vue
@@ -1,73 +1,96 @@
 <template>
-  <form class="Form" @submit.stop.prevent="onSubmit">
-    <div class="Form__Group">
-      <label class="Form__Label" for="company-name">Company Name</label>
-      <input
-        id="company-name"
-        v-model="form.name"
-        :class="{ 'Form__Input--error': isNameWrong }"
-        class="Form__Input"
-        type="text"
-        placeholder="e.g. Your Company Name"
-      />
-    </div>
-    <div v-if="isNameWrong" class="Form__Error">
-      Company Name is required.
-    </div>
-    <div class="Form__Group">
-      <label class="Form__Label" for="company-spend">Company Spend</label>
-      <input
-        id="company-spend"
-        v-model="form.spend"
-        :class="{ 'Form__Input--error': isSpendWrong }"
-        class="Form__Input"
-        type="tel"
-        placeholder="e.g. $150,000"
-        @focus="removeSpendMask()"
-        @blur="addSpendMask()"
-      />
-    </div>
-    <div v-if="isSpendWrong" class="Form__Error">
-      Company Spend is required.
-    </div>
-    <div class="Form__Group">
-      <label class="Form__Label" for="company-ability">Company Spend Ability</label>
-      <input
-        id="company-ability"
-        v-model="form.ability"
-        :class="{ 'Form__Input--error': isAbilityWrong }"
-        class="Form__Input"
-        type="tel"
-        placeholder="e.g. $150,000 - $330,000"
-        @focus="removeAbilityMask()"
-        @blur="addAbilityMask()"
-      />
-    </div>
-    <div v-if="isAbilityWrong" class="Form__Error">
-      Company Spend Ability is required and second value must be less than the first.
-    </div>
-    <div class="Form__Group">
-      <label class="Form__Label" for="company-notes">Notes</label>
-      <textarea
-        id="company-notes"
-        class="Form__Input Form__Input--textarea"
-        placeholder="e.g. Good Tech Company"
-        readonly
-        rows="6"
-        :value="form.notes"
-        @focus="openModal"
-      />
-    </div>
-  </form>
+  <div>
+    <modal name="addNotes" class="Card">
+      <form class="Form" @submit.stop.prevent="onModalSubmit">
+        <div class="Form__Group">
+          <label class="Form__Label" for="company-notes">Notes</label>
+          <textarea
+            id="company-notes"
+            v-model="modal.notes"
+            class="Form__Input Form__Input--textarea"
+            placeholder="e.g. Good Tech Company"
+            rows="6"
+          />
+        </div>
+        <div class="Form__Group">
+          <input class="Form__Button Form__Button--right" type="submit" value="Send" />
+        </div>
+      </form>
+    </modal>
+    <form class="Form" @submit.stop.prevent="onSubmit">
+      <div class="Form__Group">
+        <label class="Form__Label" for="company-name">Company Name</label>
+        <input
+          id="company-name"
+          v-model="form.name"
+          :class="{ 'Form__Input--error': isNameWrong }"
+          class="Form__Input"
+          type="text"
+          placeholder="e.g. Your Company Name"
+        />
+      </div>
+      <div v-if="isNameWrong" class="Form__Error">
+        Company Name is required.
+      </div>
+      <div class="Form__Group">
+        <label class="Form__Label" for="company-spend">Company Spend</label>
+        <input
+          id="company-spend"
+          v-model="form.spend"
+          :class="{ 'Form__Input--error': isSpendWrong }"
+          class="Form__Input"
+          type="tel"
+          placeholder="e.g. $150,000"
+          @focus="removeSpendMask()"
+          @blur="addSpendMask()"
+        />
+      </div>
+      <div v-if="isSpendWrong" class="Form__Error">
+        Company Spend is required.
+      </div>
+      <div class="Form__Group">
+        <label class="Form__Label" for="company-ability">Company Spend Ability</label>
+        <input
+          id="company-ability"
+          v-model="form.ability"
+          :class="{ 'Form__Input--error': isAbilityWrong }"
+          class="Form__Input"
+          type="tel"
+          placeholder="e.g. $150,000 - $330,000"
+          @focus="removeAbilityMask()"
+          @blur="addAbilityMask()"
+        />
+      </div>
+      <div v-if="isAbilityWrong" class="Form__Error">
+        Company Spend Ability is required and second value must be less than the first.
+      </div>
+      <div class="Form__Group">
+        <label class="Form__Label" for="company-notes">Notes</label>
+        <textarea
+          id="company-notes"
+          class="Form__Input Form__Input--textarea"
+          placeholder="e.g. Good Tech Company"
+          readonly
+          rows="6"
+          :value="form.notes"
+          @focus="openModal"
+        />
+      </div>
+    </form>
+  </div>
 </template>
 <script>
 export default {
   name: 'CompanyForm',
   data: () => ({
+    modal: {
+      notes: null
+    },
     form: {
       name: null,
       spend: null,
-      ability: null
+      ability: null,
+      notes: null
     }
   }),
   computed: {
@@ -85,11 +108,15 @@ export default {
     }
   },
   methods: {
+    onModalSubmit() {
+      this.form.notes = this.modal.notes
+      this.$modal.hide('addNotes')
+    },
     onSubmit() {
       return
     },
     openModal() {
-      console.log('lol')
+      this.$modal.show('addNotes')
     },
     rawNumber: number => number.replace(/\D+/g, '') || 0,
     addSpendMask() {
